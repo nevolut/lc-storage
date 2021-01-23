7722cae8c5277613039ceff9acbdd5f5474c570a
Archimède Body <Archimedebody@rocketmail.com>
2021-01-23 12:58:40 +0100

    updated

--- index.js
+++ index.js
@@ -1,54 +1,70 @@
-const storage = {
-  get(key, renew = null) {
-    if (!key) {
-      console.error("Key is missing");
-      return;
-    }
-    try {
-      const now = new Date().getTime();
-      const values = JSON.parse(localStorage.getItem(key));
-      if (values && values.data) {
-        if (renew) {
-          let diff = (now - values.time) / 1000;
-          diff /= 60;
-          diff = Math.abs(Math.round(diff));
-          if (diff > renew) this.remove(key);
+var storage = typeof localStorage !== "undefined" && localStorage !== null
+    ? {
+        get: function (key) {
+            if (!key) {
+                console.error("Key is missing");
+                return;
+            }
+            try {
+                var values = JSON.parse(localStorage.getItem(key));
+                if (values === null || values === void 0 ? void 0 : values.data) {
+                    if (values.expiration) {
+                        var now = new Date().getTime();
+                        var diff = (now - values.expiration) / 1000;
+                        diff /= 60;
+                        diff = Math.abs(Math.round(diff));
+                        if (diff > 0) {
+                            this.remove(key);
+                            return null;
+                        }
+                    }
+                    return values.data;
+                }
+            }
+            catch (e) {
+                this.remove(key);
+            }
+            return null;
+        },
+        set: function (key, value, expiration, nullable) {
+            if (nullable === void 0) { nullable = false; }
+            if (key == "undefined")
+                return console.error("Key is missing");
+            if (value == "undefined")
+                return console.error("Value is missing");
+            if (!value || value == {} || (Array.isArray(value) && !value.length)) {
+                if (nullable)
+                    this.remove(key);
+                return null;
+            }
+            try {
+                var now = new Date().getTime();
+                localStorage.setItem(key, JSON.stringify({
+                    data: value,
+                    time: now,
+                    expiration: expiration ? now + expiration * 1000 : null
+                }));
+            }
+            catch (e) {
+                console.error(e);
+            }
+        },
+        remove: function (key) {
+            localStorage.removeItem(key);
+        },
+        clear: function () {
+            localStorage.clear();
         }
-        return values.data;
-      }
-    } catch (e) {
-      this.remove(key);
     }
-    return null;
-  },
-
-  set(key, value, nullable = true) {
-    if (key == "undefined") return console.error("Key is missing");
-    if (value == "undefined") return console.error("Value is missing");
-    if (!value || value == {} || (Array.isArray(value) && !value.length)) {
-      if (nullable) this.remove(key);
-      return null;
-    }
-
-    try {
-      const now = new Date().getTime();
-      localStorage.setItem(key, JSON.stringify({ data: value, time: now }));
-    } catch (e) {
-      console.error(e);
-    }
-  },
-
-  remove(key) {
-    localStorage.removeItem(key);
-  },
-
-  clear() {
-    localStorage.clear();
-  }
-};
-
-storage.read = storage.get;
-storage.write = storage.set;
-storage.del = storage.remove;
-
+    : {
+        set: function () {
+            console.warn("localStorage is not defined");
+        },
+        get: function () {
+            console.warn("localStorage is not defined");
+        },
+        remove: function () {
+            console.warn("localStorage is not defined");
+        }
+    };
 module.exports = storage;
---
+++ index.ts
@@ -0,0 +1,73 @@
+const storage =
+  typeof localStorage !== "undefined" && localStorage !== null
+    ? {
+        get(key: string): Object {
+          if (!key) {
+            console.error("Key is missing");
+            return;
+          }
+          try {
+            const values = JSON.parse(localStorage.getItem(key));
+            if (values?.data) {
+              if (values.expiration) {
+                const now = new Date().getTime();
+                let diff = (now - values.expiration) / 1000;
+                diff /= 60;
+                diff = Math.abs(Math.round(diff));
+                if (diff > 0) {
+                  this.remove(key);
+                  return null;
+                }
+              }
+              return values.data;
+            }
+          } catch (e) {
+            this.remove(key);
+          }
+          return null;
+        },
+
+        set(key, value, expiration: number, nullable: boolean = false) {
+          if (key == "undefined") return console.error("Key is missing");
+          if (value == "undefined") return console.error("Value is missing");
+          if (!value || value == {} || (Array.isArray(value) && !value.length)) {
+            if (nullable) this.remove(key);
+            return null;
+          }
+
+          try {
+            const now = new Date().getTime();
+            localStorage.setItem(
+              key,
+              JSON.stringify({
+                data: value,
+                time: now,
+                expiration: expiration ? now + expiration * 1000 : null
+              })
+            );
+          } catch (e) {
+            console.error(e);
+          }
+        },
+
+        remove(key: string) {
+          localStorage.removeItem(key);
+        },
+
+        clear() {
+          localStorage.clear();
+        }
+      }
+    : {
+        set: () => {
+          console.warn("localStorage is not defined");
+        },
+        get: () => {
+          console.warn("localStorage is not defined");
+        },
+        remove: () => {
+          console.warn("localStorage is not defined");
+        }
+      };
+
+module.exports = storage;
